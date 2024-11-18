1dd8fea844bf75bc9818d3137d8a296063b95317
unknown <sajjadpoostini007@gmail.com>
2024-11-18 12:54:15 -0800

    finsweet

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./blogpage.html"],
+  theme: {
+    extend: {
+      fontFamily:{"poppins":"poppins"}
+    },
+  },
+  plugins: [],
+}
+
